48cfdaba3be7b4059351a259d90ce6a0978eb023
anderson <andersonfelpe123@gmail.com>
2022-04-05 12:48:35 -0300

    secondCommit

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const btn = document.querySelector('.btn')
 const container = document.querySelector('.container')
+const txt = document.querySelector('.texts')
 
 btn.onclick = function(){
     this.classList.toggle('active')
     container.classList.toggle('active')
+    txt.classList.toggle('active') 
+    if(txt.classList.contains('active')){
+        txt.textContent = 'Dark'
+    }else{
+        txt.textContent = 'Light'
+    } 
 }
